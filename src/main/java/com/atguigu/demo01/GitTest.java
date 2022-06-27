6a0d67b980cfb10a60c086c10f51d9cea72d8ab2
Hunter <hunter@cumt.com>
2022-06-27 18:14:15 +0800

    third commit

--- src/main/java/com/atguigu/demo01/GitTest.java
+++ src/main/java/com/atguigu/demo01/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git");
         System.out.println("第二次修改...");
+        System.out.println("第三次修改...");
     }
 }
