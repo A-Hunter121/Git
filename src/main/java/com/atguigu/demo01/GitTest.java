5ee4ee7234a55b1b4cef9886126c1b6ada32dad5
Hunter <hunter@cumt.com>
2022-06-27 18:13:05 +0800

    second commit

--- src/main/java/com/atguigu/demo01/GitTest.java
+++ src/main/java/com/atguigu/demo01/GitTest.java
@@ -3,5 +3,6 @@ package com.atguigu.demo01;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git");
+        System.out.println("第二次修改...");
     }
 }
