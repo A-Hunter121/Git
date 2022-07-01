a34483327322e261e9ec9c4bde5759d1b751de76
Hunter <hunter@cumt.com>
2022-07-01 21:14:20 +0800

    master fourth commit

--- src/main/java/com/atguigu/demo01/GitTest.java
+++ src/main/java/com/atguigu/demo01/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("第二次修改...");
         System.out.println("第三次修改...");
         System.out.println("在hot-fix分支进行的修改");
+        System.out.println("测试代码冲突");
     }
 }
